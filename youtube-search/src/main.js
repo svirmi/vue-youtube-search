db6dc2f47837b7c9490b90a3b08c66ead4792b14
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-01-30 08:52:15 +0200

    use createElement function alias

--- youtube-search/src/main.js
+++ youtube-search/src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App";
 
 new Vue({
-    render: function (createElement) {
-        return createElement(App);
+    render: function (h) {
+        return h(App);
     }
 });
