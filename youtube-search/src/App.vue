5de36db36c2411cf9569287241c0957db39e0644
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-05 08:46:40 +0200

    axios installed and plugged-in

--- youtube-search/src/App.vue
+++ youtube-search/src/App.vue
@@ -5,14 +5,24 @@
 </template>
 
 <script>
+    import axios from 'axios';
     import SearchBar from "./components/SearchBar";
+
+    const API_KEY = process.env.VUE_APP_API_KEY;
 
     export default {
         name: "App",
         components: {SearchBar},
         methods: {
             onTermChange(searchTerm) {
-                console.log(searchTerm);
+                axios.get('https://googleapis.com/youtube/v3/search',{
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => console.log(response));
             }
         }
     }
