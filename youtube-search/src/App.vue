57aaa56275615601d033016094f6a681edc50d48
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-05 09:03:35 +0200

    VideoList component setup

--- youtube-search/src/App.vue
+++ youtube-search/src/App.vue
@@ -1,21 +1,23 @@
 <template>
     <div>
         <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
     </div>
 </template>
 
 <script>
     import axios from 'axios';
     import SearchBar from "./components/SearchBar";
+    import VideoList from "./components/VideoList";
 
     const API_KEY = process.env.VUE_APP_API_KEY;
 
     export default {
         name: "App",
-        components: {SearchBar},
+        components: {SearchBar, VideoList},
         methods: {
             onTermChange(searchTerm) {
-                axios.get('https://googleapis.com/youtube/v3/search',{
+                axios.get('https://www.googleapis.com/youtube/v3/search',{
                     params: {
                         key: API_KEY,
                         type: 'video',
