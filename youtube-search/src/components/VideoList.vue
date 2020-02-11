64d8cc4027af723590988e76bcd191fde981fbf9
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-11 09:09:14 +0200

    VideoList && VideoListItems

--- youtube-search/src/components/VideoList.vue
+++ youtube-search/src/components/VideoList.vue
@@ -1,12 +1,23 @@
 <template>
     <ul>
-        VideoList
+        <VideoListItem
+                v-for="video in videos"
+                :video="video"
+                :key="video.etag"
+        >
+        </VideoListItem>
     </ul>
 </template>
 
 <script>
+    import VideoListItem from "./VideoListItem";
+
     export default {
-        name: "VideoList"
+        name: "VideoList",
+        components: {VideoListItem},
+        props: {
+            videos: Array
+        }
     }
 </script>
 
---
+++ youtube-search/src/components/VideoListItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        {{ video.snippet.title }}
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "VideoListItem",
+        props: {
+            video: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
