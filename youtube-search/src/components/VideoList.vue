eb3460ee50f9eff92b3938719b4223dc47fb8cd5
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-13 09:12:58 +0200

    emmiting an event from videoListItem component up to VideoList component

--- youtube-search/src/components/VideoList.vue
+++ youtube-search/src/components/VideoList.vue
@@ -4,6 +4,7 @@
                 v-for="video in videos"
                 :video="video"
                 :key="video.etag"
+                @videoSelect="onVideoSelect"
         >
         </VideoListItem>
     </ul>
@@ -17,6 +18,11 @@
         components: {VideoListItem},
         props: {
             videos: Array
+        },
+        methods: {
+            onVideoSelect(video) {
+                this.$emit('videoSelect', video)
+            }
         }
     }
 </script>
