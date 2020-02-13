8de9f8161f8e1a36e7e91a53d09db278ab32d88e
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-02-13 08:50:39 +0200

    list items styling

--- youtube-search/src/components/VideoListItem.vue
+++ youtube-search/src/components/VideoListItem.vue
@@ -1,7 +1,9 @@
 <template>
-    <li class="list-group-item">
-        <img :src="thumbnailUrl" />
-        {{ video.snippet.title }}
+    <li class="list-group-item media">
+        <img class="mr-3" :src="thumbnailUrl" />
+        <div class="media-body">
+            {{ video.snippet.title }}
+        </div>
     </li>
 </template>
 
@@ -20,5 +22,11 @@
 </script>
 
 <style scoped>
-
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+    li:hover {
+        background-color: #eee;
+    }
 </style>
